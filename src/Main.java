4f666552498a0d1ec044490693eb747827492550
Carlos <carlos.m.melo2003@gmail.com>
2024-10-28 18:10:46 -0300

    Criado a função calculoDosPontos que ainda esta incompleta e outro para a impresão desta função para teste que sera apagada dps. Adcionado mais tipos para impresão na função navios como bombas, navios grande etc. Proxima priopridade e calculo do dos pontos do jogo e implentar a maquina e jagador2 (talvez).

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,16 @@ public class Main {
     static Scanner ler = new Scanner(System.in);
     public static final String resetar = "\u001B[0m";
     public static final String vermelho = "\u001B[31m";
-
+    public static int dificuldade;
     public static void main(String[] args) {
+
         // Inicialização de variáveis
         int[] dimensoes;
         int linha1, coluna1, tabus;
         String tabB = "0123456789";
-        int dificuldade;
 
-        // Seleção da dificuldade
-        do {
-            System.out.println("Informe a dificuldade\n1. 3 tabuleiros de 5x5\n2. 3 tabuleiros de 10x10");
-            dificuldade = ler.nextInt();
-        } while (dificuldade > 2 || dificuldade < 1);
+        // Chamando a seleção da dificuldade
+        difculdade();
 
         dimensoes = difi(dificuldade);
         linha1 = dimensoes[0];
@@ -32,11 +29,22 @@ public class Main {
 
         // Inicialização dos tabuleiros e posicionamento dos navios
         inicializarTabuleiros(tabuleiro, backJogo, tabus, linha1, coluna1);
-        navios(tabuleiro, 18, dificuldade);  // Posiciona 5 navios nos tabuleiros
+        navios(tabuleiro, 5,5,5,5, dificuldade);  // Posiciona 5 navios nos tabuleiros
+
+        //troca os * por 0 e N por 1 para fazer o calculo depois (incompleto), exibirBackJogo exibi os numeros para propositos de teste
+        calculoDosPontos(backJogo, tabuleiro, tabus, linha1, coluna1);
+        exibirBackJogo(backJogo, tabus, linha1, coluna1);
 
         // Início do jogo
         System.out.println("Você tem 20 jogadas para tentar acertar os alvos");
         executarJogadas(tabuleiro, backJogo, tabB, tabus, linha1, coluna1);
+    }
+
+    public static void difculdade(){
+        do {
+            System.out.println("Informe a dificuldade\n1. 3 tabuleiros de 5x5\n2. 3 tabuleiros de 10x10");
+            dificuldade = ler.nextInt();
+        } while (dificuldade > 2 || dificuldade < 1);
     }
 
     // Função de configuração da dificuldade
@@ -109,11 +117,15 @@ public class Main {
     }
 
     // Função para posicionar navios no tabuleiro
-    public static void navios(String[][][] tabuleiro, int naviosTotais, int dificuldade) {
+    public static void navios(String[][][] tabuleiro, int naviosTotais, int bombasTotais, int naviosGrandeTotais, int suprimentosTotais, int dificuldade) {
         int naviosNoTabuleiro = 0;
+        int bombasNoTabuleiro = 0;
+        int naviosGrandeNoTabuleiro = 0;
+        int suprimentosNoTabuleiro = 0;
         int linha, coluna, camada;
+        String[] tipos = {" S ", " B ", " N ", " n "};
 
-        while (naviosNoTabuleiro < naviosTotais) {
+        while (naviosNoTabuleiro < naviosTotais || bombasNoTabuleiro < bombasTotais || naviosGrandeNoTabuleiro < naviosGrandeTotais || suprimentosNoTabuleiro < suprimentosTotais) {
             camada = rand.nextInt(3);  // Escolhe uma camada aleatória sd
             if (dificuldade == 1) {
                 linha = rand.nextInt(5);
@@ -123,11 +135,64 @@ public class Main {
                 coluna = rand.nextInt(10);
             }
 
-            // Verifica se a posição está livre antes de posicionar um navio
             if (tabuleiro[camada][linha][coluna].equals(" * ")) {
-                tabuleiro[camada][linha][coluna] = " N "; // Coloca um navio
-                naviosNoTabuleiro++;
+                // Determina o que colocar, se necessário
+                if (naviosNoTabuleiro < naviosTotais) {
+                    tabuleiro[camada][linha][coluna] = " n "; // Coloca um navio
+                    naviosNoTabuleiro++;
+                } else if (bombasNoTabuleiro < bombasTotais) {
+                    tabuleiro[camada][linha][coluna] = " B "; // Coloca uma bomba
+                    bombasNoTabuleiro++;
+                } else if (naviosGrandeNoTabuleiro < naviosGrandeTotais) {
+                    tabuleiro[camada][linha][coluna] = " N "; // Coloca um navio grande
+                    naviosGrandeNoTabuleiro++;
+                } else if (suprimentosNoTabuleiro < suprimentosTotais) {
+                    tabuleiro[camada][linha][coluna] = " S "; // Coloca suprimentos
+                    suprimentosNoTabuleiro++;
+                }
             }
+        }
+    }
+
+    //troca a String por numeros nos valores especificos
+    public static int[][][] calculoDosPontos (int[][][] backJogo, String[][][] tabuleiro, int tabus, int linha1, int coluna1) {
+        for (int i = 0; i < tabus; i++) {
+            for (int j = 0; j < linha1; j++) {
+                for (int k = 0; k < coluna1; k++) {
+                    if (tabuleiro[i][j][k].equals(" * ")) {
+                        backJogo[i][j][k] = 0;
+                    }
+                    if (tabuleiro[i][j][k].equals(" n ")) {
+                        backJogo[i][j][k] = 1;
+                    }
+                    if (tabuleiro[i][j][k].equals(" N ")) {
+                        backJogo[i][j][k] = 1;
+                    }
+                    if (tabuleiro[i][j][k].equals(" S ")) {
+                        backJogo[i][j][k] = 4;
+                    }
+                    if (tabuleiro[i][j][k].equals(" B ")) {
+                        backJogo[i][j][k] = -3;
+                    }
+
+                }
+            }
+        }
+        return backJogo;
+    }
+
+
+    //imprimi imprimindo a função calcular pontos para propositos de teste
+    public static void exibirBackJogo(int[][][] backJogo, int tabus, int linha1, int coluna1) {
+        for (int i = 0; i < tabus; i++) {
+            System.out.println("Camada " + i + ":");
+            for (int j = 0; j < linha1; j++) {
+                for (int k = 0; k < coluna1; k++) {
+                    System.out.print(backJogo[i][j][k] + " ");  // Imprime o valor na posição [i][j][k]
+                }
+                System.out.println();  // Nova linha após imprimir uma linha do tabuleiro
+            }
+            System.out.println();  // Nova linha após imprimir uma camada
         }
     }
 }
